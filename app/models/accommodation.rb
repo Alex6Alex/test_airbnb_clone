fe5bab2cfd8b8c051dcf8fb281403afc7dc2a16e
Alex <>
2023-02-26 14:49:06 +0400

    add new views for accommodations

--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -4,4 +4,6 @@ class Accommodation < ApplicationRecord
   belongs_to :user
 
   validates :name, :city, :country, :person_count, :bed_count, :description, presence: true
+
+  delegate :username, to: :user, prefix: true
 end
